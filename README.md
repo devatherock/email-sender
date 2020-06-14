2aa719501231577f58441a0e581e2be33cd9be9f
devaprasadh <devaprasadh.xavier@target.com>
2020-06-14 10:02:46 -0500

    Code cleanup - added comments and validation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# email-sender-api
-REST API to send emails
+[![CircleCI](https://circleci.com/gh/devatherock/email-sender.svg?style=svg)](https://circleci.com/gh/devatherock/email-sender)
+[![Docker Pulls](https://img.shields.io/docker/pulls/devatherock/email-sender.svg)](https://hub.docker.com/r/devatherock/email-sender/)
+[![Docker Image Size](https://img.shields.io/docker/image-size/devatherock/email-sender.svg?sort=date)](https://hub.docker.com/r/devatherock/email-sender/)
+[![Docker Image Layers](https://img.shields.io/microbadger/layers/devatherock/email-sender.svg)](https://microbadger.com/images/devatherock/email-sender)
+# email-sender
+REST API to send emails. Sends the email using the configured SMTP server.
+
+## API Reference
+Refer the swagger [spec](https://smtp-email-sender-api.herokuapp.com/swagger/v3/api-docs) or the swagger 
+[UI](https://smtp-email-sender-api.herokuapp.com/swagger-ui.html)
+
+## Configurable properties
+
+| Name                                  |   Type       |   Default        |   Description                                                                     |
+|---------------------------------------|--------------|------------------|-----------------------------------------------------------------------------------|
+| emailsender.smtp.embedded             |   boolean    |   false          |   Indicates if an embedded SMTP server should be used. Turned off by default      |
+| emailsender.smtp.username             |   String     |   (None)         |   The SMTP username                                                               |
+| emailsender.smtp.password             |   String     |   (None)         |   The SMTP password                                                               |
+| emailsender.smtp.host                 |   String     |   (None)         |   The SMTP server name                                                            |
+| emailsender.smtp.port                 |   int        |   587            |   The SMTP port. Defaults to 587                                                  |
+| emailsender.smtp.from-address.name    |   String     |   (None)         |   The from name to use in outbound emails                                         |
+| emailsender.smtp.from-address.email   |   String     |   (None)         |   The from email address to use in outbound emails                                |
---
+++ src/main/java/io/github/devatherock/emailsender/EmailSenderProperties.java
@@ -0,0 +1,93 @@
+package io.github.devatherock.emailsender;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Configurable properties for the application
+ */
+@ConfigurationProperties("emailsender")
+@Getter
+@Setter
+@Component
+@Validated
+public class EmailSenderProperties {
+    @Valid
+    private SmtpProperties smtp = new SmtpProperties();
+
+    /**
+     * SMTP configuration
+     */
+    @Getter
+    @Setter
+    public static class SmtpProperties {
+        /**
+         * Indicates if an embedded SMTP server should be used. Turned off by default
+         */
+        private boolean embedded = false;
+        /**
+         * The SMTP username
+         */
+        private String username;
+        /**
+         * The SMTP username
+         */
+        private String password;
+        /**
+         * The SMTP server name
+         */
+        private String host;
+        /**
+         * The SMTP port. Defaults to 587
+         */
+        private int port = 587;
+
+        /**
+         * The from address to use in outbound emails
+         */
+        @Valid
+        private Contact fromAddress;
+    }
+
+    /**
+     * Validates if all required attributes are specified
+     *
+     * @return
+     */
+    @AssertTrue(message = "SMTP server details missing")
+    public boolean isValidSmtpConfig() {
+        return smtp.embedded || !(StringUtils.isEmpty(smtp.username) || StringUtils.isEmpty(smtp.password)
+                || StringUtils.isEmpty(smtp.host));
+    }
+
+    /**
+     * Class representing a contact
+     */
+    @Getter
+    @Setter
+    public static class Contact {
+        /**
+         * The name of the recipient. Optional
+         */
+        @Schema(description = "The name of the recipient. Optional", example = "Test.User")
+        private String name;
+
+        /**
+         * The email address of the recipient. Required
+         */
+        @Email
+        @NotBlank
+        @Schema(description = "The email address of the recipient. Required", example = "test@test.com")
+        private String email;
+    }
+}
